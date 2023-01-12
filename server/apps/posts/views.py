8b90d847111f565e94c8497f0cafa26eaa1d7028
hongcana <hongcana@kakao.com>
2023-01-12 15:26:46 +0900

    posts 기능 개발중

--- server/apps/posts/views.py
+++ server/apps/posts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from server.apps.posts.models import Post
 
-# Create your views here.
+# args, keyword args(kwargs)
+
+def hello_world(request, *args, **kwargs):
+    return render(request, "posts/hello_world.html")
+
+def posts_list(request, *args, **kwargs):
+    posts = Post.objects.all()
+    print({"posts":posts})
+    return render(request, "posts/posts_list.html", {"posts":posts})
+    # context = template으로 데이터를 전달
+
+def posts_retrieve(request, pk, *args, **kwargs):
+    post = Post.objects.all().get(pk=pk)
+    return render(request, "posts/posts_retrieve.html", {"post":post})
+
+def posts_create(request, *args, **kwargs):
+    if request.method == "POST":
+        Post.objects.create(
+            title = request.POST["title"],
+            user = request.POST["user"],
+            region = request.POST["region"],
+            price = request.POST["price"],
+            content = request.POST["content"],
+        )
+        return redirect("/")
+    return render(request, "posts/posts_create.html")
+
+def posts_delete(request, *args, **kwargs):
+    return redirect("/")
