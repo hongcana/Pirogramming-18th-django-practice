931468ad498c7ddde3797880b35ba9a35ea35bc3
hongcana <hongcana@kakao.com>
2023-01-12 11:41:25 +0900

    add posts app

---
+++ server/apps/posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# 데이터베이스와 연결되는 python의 class
+
+class Post(models.Model):
+    title = models.CharField(max_length=32)
+    user = models.CharField(max_length=32)
+    # user는 실제 프로젝트 개발시 CharField X
+    content = models.TextField() # 글자수 제한이 없는 Field
+    region = models.CharField(max_length=16)
+    price = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
