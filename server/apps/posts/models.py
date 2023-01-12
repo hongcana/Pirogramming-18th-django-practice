60f17721f5089b51856c3af5647ebfa108b4fccc
hongcana <hongcana@kakao.com>
2023-01-12 11:57:35 +0900

    update model

--- server/apps/posts/models.py
+++ server/apps/posts/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # 데이터베이스와 연결되는 python의 class
 
 class Post(models.Model):
-    title = models.CharField(max_length=32)
+    title = models.CharField(max_length=64)
     user = models.CharField(max_length=32)
     # user는 실제 프로젝트 개발시 CharField X
-    content = models.TextField() # 글자수 제한이 없는 Field
+    content = models.TextField()
     region = models.CharField(max_length=16)
     price = models.IntegerField()
 
